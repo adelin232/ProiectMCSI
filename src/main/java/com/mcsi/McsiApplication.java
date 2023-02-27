a56bb0dc6a9c8913d8b3d088aaa4fb7069dd0a1c
adelin232 <narcis.adelin.miulet@gmail.com>
2023-02-27 22:56:09 +0200

    Create boilerplate app using Spring Initializr

---
+++ src/main/java/com/mcsi/McsiApplication.java
@@ -0,0 +1,13 @@
+package com.mcsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcsi/McsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
